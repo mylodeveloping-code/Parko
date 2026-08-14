ac84450623087959f40ef24f3fb4b38a62b4893b
mylodeveloping-code <mylodeveloping@gmail.com>
2026-08-14 12:38:42 -0400

    Fix formatting in untimeout.js

--- src/commands/Moderation/untimeout.js
+++ src/commands/Moderation/untimeout.js
@@ -1,4 +1,4 @@
-```javascript
+javascript
 import { SlashCommandBuilder, PermissionFlagsBits } from 'discord.js';
 import { successEmbed } from '../../utils/embeds.js';
 import { logger } from '../../utils/logger.js';
