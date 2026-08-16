a56d14267b43311fe86e2d69db55ff5613c87add
Voo <mylodeveloping@gmail.com>
2026-08-15 20:52:17 -0400

    Update unblacklist.js

--- src/utils/unblacklist.js
+++ src/utils/unblacklist.js
@@ -8,14 +8,42 @@ import {
     isBlacklisted,
 } from '../../utils/blacklist.js';
 
+function resolveUserId(value) {
+    if (!value) {
+        return null;
+    }
+
+    const stringValue =
+        String(value).trim();
+
+    const mentionMatch =
+        stringValue.match(
+            /^<@!?(\d+)>$/
+        );
+
+    if (mentionMatch) {
+        return mentionMatch[1];
+    }
+
+    if (/^\d{17,20}$/.test(stringValue)) {
+        return stringValue;
+    }
+
+    return null;
+}
+
 export default {
     data: new SlashCommandBuilder()
         .setName('unbl')
-        .setDescription('Remove a user from the bot blacklist')
+        .setDescription(
+            'Remove a user from the bot blacklist'
+        )
         .addStringOption((option) =>
             option
                 .setName('user_id')
-                .setDescription('The Discord user ID to unblacklist')
+                .setDescription(
+                    'The Discord user ID to unblacklist'
+                )
                 .setRequired(true)
         )
         .setDefaultMemberPermissions(
@@ -24,32 +52,51 @@ export default {
 
     category: 'moderation',
 
-    async execute(interaction, config, client) {
+    async execute(
+        interaction,
+        config,
+        client
+    ) {
+        const rawUserId =
+            interaction.options.getString(
+                'user_id'
+            );
+
         const userId =
-            interaction.options.getString('user_id');
+            resolveUserId(
+                rawUserId
+            );
 
         if (!userId) {
             await interaction.reply(
-                'Usage: `.unbl <user ID>`'
-            );
-            return;
-        }
-
-        if (!/^\d{17,20}$/.test(userId)) {
-            await interaction.reply(
                 'Please provide a valid Discord user ID.'
             );
+
             return;
         }
 
-        if (!isBlacklisted(userId)) {
+        if (
+            !isBlacklisted(userId)
+        ) {
             await interaction.reply(
                 `<@${userId}> is not currently blacklisted.`
             );
+
             return;
         }
 
-        removeFromBlacklist(userId);
+        const removed =
+            removeFromBlacklist(
+                userId
+            );
+
+        if (!removed) {
+            await interaction.reply(
+                'I could not remove that user from the blacklist.'
+            );
+
+            return;
+        }
 
         await interaction.reply(
             `<@${userId}> has been removed from the blacklist.`
